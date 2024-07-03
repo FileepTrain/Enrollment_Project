da91628942971470ab12465f88162611378b3e1b
CDMChris <chrismurillo@rocketmail.com>
2024-07-02 22:15:43 -0700

    Created Student Class

---
+++ Student.py
@@ -0,0 +1,69 @@
+from mongoengine import *
+# from studentMajor import studentMajor
+# from enrollment import enrollment
+
+
+class Student(Document):
+    lastName = StringField(db_field='last_name', max_length=50, required=True)
+    firstName = StringField(db_field='first_name', max_length=50, required=True)
+    email = StringField(db_field='email', min_lenth=10, max_length=100, required=True)
+    enrollments = ListField(ReferenceField('enrollment'))
+    studentMajors = ListField(ReferenceField('studentMajor'))
+
+    meta = {
+        'collection': 'students',
+        'indexes': [
+            {
+                'fields': ['lastName', 'firstName, email'],
+                'unique': True,
+                'name': 'last_first_name_unique'
+            }
+        ]
+    }
+
+    def add_enrollment(self, enrollment):
+        for already_enrolled in self.enrollments:
+            if enrollment.departmentAbbrevation.equals(already_enrolled.departmentAbbrevation):
+                if enrollment.courseNumber.equals(already_enrolled.courseNumber):
+                    return  # Already enrolled, don't add it.
+        self.enrollments.append(enrollment)
+
+    def remove_enrollment(self, enrollment):
+        for already_enrolled in self.enrollments:
+            if enrollment.departmentAbbrevation.equals(already_enrolled.departmentAbbrevation):
+                if enrollment.courseNumber.equals(already_enrolled.courseNumber):
+                    self.enrollments.remove(enrollment)
+                    enrollment.delete(enrollment)
+                    return
+
+    def add_major(self, studentMajor):
+        for already_enrolled in self.studentMajors:
+            if studentMajor.majorName.equals(already_enrolled.majorName):
+                return  # Already enrolled, don't add it.
+        self.studentMajors.append(studentMajor)
+
+    def remove_major(self, studentMajor):
+        for already_enrolled in self.studentMajors:
+            if studentMajor.majorName.equals(already_enrolled.majorName):
+                self.studentMajors.remove(studentMajor)
+                studentMajor.delete(studentMajor)
+                return
+
+    def __init__(self, lastName: str, firstName: str, *args, **values):
+        super().__init__(*args, **values)
+        if self.enrollments is None:
+            self.enrollments = []  # initialize to no enrollments.
+        self.firstName = firstName
+        self.lastName = lastName
+
+    def __str__(self):
+        results = f'{self.firstName}  {self.lastName} \nEmail: {self.email}'
+        if self.studentMajors:
+            results = results + f'\nCurrently studying:'
+            for major in self.studentMajors:
+                results = results + '\n\t' + f'{major.name}'
+            if self.enrollments:
+                results = results + f'Currently enrolled in:'
+            for enrollment in self.enrollments:
+                results = results + '\n\t' + f'{enrollment.department_abbreviation} {enrollment.course_number} Section {enrollment.sectionNumber}'
+        return results
