982bdd4dd01456915640095b7f1fc8ae2bf2dd54
huyvu203 <168234998+huyvu203@users.noreply.github.com>
2024-07-06 15:47:25 -0700

    add section methods to common utilities

--- CommonUtilities.py
+++ CommonUtilities.py
@@ -11,15 +11,14 @@ from Utilities import Utilities
 from ConstraintUtilities import select_general, unique_general, prompt_for_date
 from Menu import Menu
 from Option import Option
+from Section import Section
 
 
-def select_order() -> Order:
-    return select_general(Order)
+def select_course() -> Course:
+    return select_general(Course)
 
-
-def select_product() -> Product:
-    return select_general(Product)
-
+def select_section() -> Section:
+    return select_general(Section)
 
 def select_department() -> Department:
     return select_general(Department)
@@ -49,111 +48,8 @@ def prompt_for_enum(prompt: str, cls, attribute_name: str):
         raise ValueError(f'This attribute is not an enum: {attribute_name}')
 
 
-def add_order():
-    """
-    Create a new Order instance.
-    :return: None
-    """
-    success: bool = False
-    new_order = None
-    while not success:
-        order_date = prompt_for_date('Date and time of the order: ')
-        """This is sort of a hack.  The customer really should come from a Customer class, and the 
-        clerk who made the sale, but I'm trying to keep this simple to concentrate on relationships."""
-        new_order = Order(input('Customer name --> '),
-                          order_date,
-                          input('Clerk who made the sale --> '))
-        violated_constraints = unique_general(new_order)
-        if len(violated_constraints) > 0:
-            for violated_constraint in violated_constraints:
-                print('Your input values violated constraint: ', violated_constraint)
-            print('try again')
-        else:
-            # The first "stats change" is placing the order itself.
-            new_order.change_status(StatusChange(
-                prompt_for_enum('Select the status:', StatusChange, 'status'),
-                order_date))
-            try:
-                new_order.save()
-                success = True
-            except Exception as e:
-                print('Errors storing the new order:')
-                print(Utilities.print_exception(e))
-
-
-def update_order():
-    """
-    Change the status of an existing order by adding another element to the status vector of the order.
-    :return: None
-    """
-    success: bool = False
-    # "Declare" the order variable, more for cosmetics than anything else.
-    order: Order
-    while not success:
-        order = select_order()  # Find an order to modify.
-        status_change_date = prompt_for_date('Date and time of the status change: ')
-        new_status = prompt_for_enum('Select the status:', StatusChange, 'status')
-        try:
-            order.change_status(StatusChange(new_status, status_change_date))
-            order.save()
-            success = True
-        except ValueError as VE:
-            print('Attempted status change failed because:')
-            print(VE)
-
-
-def delete_order():
-    """
-    Delete an existing order from the database.
-    :return: None
-    """
-    order = select_order()  # prompt the user for an order to delete
-    items = order.orderItems  # retrieve the list of items in this order
-    for item in items:
-        """The reference from OrderItem back up to Order has a reverse_delete_rule of DENY, which 
-        is similar to the RESTRICT option on a relational foreign key constraint.  Which means that
-        if I try to delete the order and there are still any OrderItems depending on that order,
-        MongoEngine (not MongoDB) will throw an exception."""
-        item.delete()
-    # Now that all the items on the order are removed, we can safely remove the order itself.
-    order.delete()
-
-
-def add_product():
-    """
-    Create a new Product instance.
-    :return: None
-    """
-    success: bool = False
-    new_product = None
-    while not success:
-        product_code = input('Enter Product Code --> ')
-        product_name = input('Enter the product name --> ')
-        quantity_in_stock = int(input('Enter Quantity in Stock --> '))
-        product_description = input('Enter Product Description --> ')
-        product_price = decimal.Decimal(input('Enter Product Price --> '))
-        msrp = decimal.Decimal(input('Enter MSRP --> '))
-        new_product = Product(product_code,
-                              product_name,
-                              product_description, quantity_in_stock, product_price, msrp)
-
-        violated_constraints = unique_general(new_product)
-        if len(violated_constraints) > 0:
-            for violated_constraint in violated_constraints:
-                print('Your input values violated constraint: ', violated_constraint)
-            print('try again')
-        else:
-            try:
-                new_product.save()
-                success = True
-            except Exception as e:
-                print('Errors storing the new order:')
-                print(Utilities.print_exception(e))
-
-
 def add_department():
     success: bool = False
-    new_product = None
     while not success:
         name = input('Enter Department Name --> ')
         abbreviation = input('Enter the Department Abbreviation --> ')
@@ -222,6 +118,7 @@ def add_course():
                 print('Exception trying to add the new course:')
                 print(Utilities.print_exception(e))
 # delete a course from a department
+
 def delete_course():
     department = select_department()
     all_courses = department.courses    # the list of courses in this department
@@ -240,16 +137,56 @@ def list_course():
         print(course)
  
 
-def delete_product():
-    """
-    Delete an existing product from the database.
-    :return: None
-    """
-    product = select_product()  # prompt the user for an order to delete
-    orderItems = product.orderItems
+# add a section to a course
+def add_section():
+    success: bool = False
+    new_section: Section
+    course: Department
+    while not success:
+        course = select_course() # select a course 
+        section_number = int(input("Enter Section Number --> "))
+        semester = prompt_for_enum("Enter Semester (Fall, Spring, Summer I, Summer II, Summer III, Winter) --> ", Section, 'semester')
+        section_year = int(input("Enter Year --> "))
+        building = prompt_for_enum("Enter Building (ANAC, CDC, DC, ECS, EN2, EN3, EN4, EN5, ET, HSCI, NUR, VEC) --> ", Section, 'building')
+        room = int(input("Enter Room Number (> 1 and < 1000) --> "))
+        schedule = prompt_for_enum("Enter Schedule (MW, TuTh, MWF, F, S) --> ", Section, 'schedule')
+        start_time = prompt_for_enum("Enter Section Start Time (HH:MM, 24-hour format, >= 08:00 and <= 19:30) --> ", Section, 'startTime')
+        instructor = input("Enter Instructor Name --> ")
+        # create a new section
+        new_section = Section(course, section_number, semester, section_year, building, room, schedule, start_time, instructor)
+        # check unique
+        violated_constraints = unique_general(new_section)
+        if len(violated_constraints) > 0:
+            for violated_constraint in violated_constraints:
+                print('Your input values violated constraint: ', violated_constraint)
+            print('Try again')
+        else:
+            try:
+                new_section.save()  # save new section
+                success = True
+            except Exception as e:
+                print('Exception trying to add the new course:')
+                print(Utilities.print_exception(e))
 
-    for orderItem in orderItems:
-        orderItem.delete()
+# delete a section from a course
+def delete_section():
+    section = select_section()
+    # check for enrollments
+    if section.enrollments:
+        print("This section cannot be deleted because it has students enrolled in it.")
+        return
+    # delete the section
+    try:
+        section.delete()
+        print(f'Section {section.sectionNumber} has been successfully deleted.')
+    except Exception as e:
+        print('Errors deleting section:')
+        print(Utilities.print_exception(e))    
 
-    product.delete()
+# list all sections of a course
+def list_section():
+    course = select_course()
+    all_sections = Section.objects(course=course) # get all section within that course
+    for section in all_sections:
+        print(section)
 
